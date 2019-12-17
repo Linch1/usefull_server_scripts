7356fbeb66dabda7a0d4140e46987983f100a25b
Linch1 <47044850+Linch1@users.noreply.github.com>
2019-12-17 12:21:12 +0100

    Add files via upload

---
+++ linux_basic_server_setup.py
@@ -0,0 +1,23 @@
+import os
+
+# download dockers
+os.system('sudo apt-get remove docker docker-engine docker.io containerd runc')
+os.system('sudo apt-get update')
+os.system('sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common')
+os.system('curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -')
+os.system('sudo apt-key fingerprint 0EBFCD88')
+os.system('sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"')
+os.system('sudo apt-get update')
+os.system('sudo apt-get install docker-ce docker-ce-cli containerd.io')
+os.system('sudo docker run hello-world')
+
+# downlaod github
+os.system('sudo apt install git')
