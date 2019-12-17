54ecf4b3eeca13c0627d5378653e04dfc0837567
Linch1 <47044850+Linch1@users.noreply.github.com>
2019-12-17 12:23:27 +0100

    Update linux_basic_server_setup.py

--- linux_basic_server_setup.py
+++ linux_basic_server_setup.py
@@ -2,7 +2,7 @@ import os
 
 #update
 os.system('sudo apt-get update')
-os.system('sudo apt-get upgrade')
+os.system('sudo apt-get upgrade -y')
 
 # download dockers
 os.system('sudo apt-get remove docker docker-engine docker.io containerd runc')
@@ -11,7 +11,7 @@ os.system('sudo apt-get install \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common')
+    software-properties-common -y')
 os.system('curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -')
 os.system('sudo apt-key fingerprint 0EBFCD88')
 os.system('sudo add-apt-repository \
@@ -19,8 +19,8 @@ os.system('sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"')
 os.system('sudo apt-get update')
-os.system('sudo apt-get install docker-ce docker-ce-cli containerd.io')
+os.system('sudo apt-get install docker-ce docker-ce-cli containerd.io -y')
 os.system('sudo docker run hello-world')
 
 # downlaod github
-os.system('sudo apt install git')
+os.system('sudo apt install git -y')
